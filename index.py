efc5ddbc6cf40011344081e33b74e717c36d8d4f
Siddharth Soni <54449601+siddharthsonii@users.noreply.github.com>
2020-09-20 10:48:35 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,19 @@
+import turtle   
+
+wn=turtle.Screen()
+
+wn.bgcolor("black")
+
+colors = ["red","purple","blue","green","orange","yellow"]
+
+draw = turtle.Turtle()
+
+
+for x in range(360):
+    draw.pencolor(colors[x%6])
+    draw.width(x/100+1)
+    draw.forward(x)
+    draw.left(59)
+
+turtle.done()
+
